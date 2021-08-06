c2ef3696cbe5728a0fe933c099155c171bd5d522
Hayk Yolyan <54978371+BaloraTheRuiner@users.noreply.github.com>
2021-08-06 16:50:38 +0400

    Add files via upload

---
+++ operations/eyelid_surgery/index.php
@@ -0,0 +1,35 @@
+<?php include "../header.php"; ?>
+
+			
+	<div class="wrap_div">
+		<div class="wrap_diamond operations_wrap_diamond">
+			<div class="eyelid_diamond_icon wrap_diamond_icon oper_icon"></div>
+		</div>
+		<?php foreach ($db->operationsTitles() as $key){ if($key["id"] == 8){ ?>
+		<div id="eyelid_id" class="wrap_line"><b><?=$key["title_" . $_SESSION["NowLang"] . ""]?></b></div>
+	</div>
+
+	<div class="operations">
+		<div class="inner_operations">
+
+
+
+
+			<div class="operations_content">
+				<div class="operations_content_div"><img width="350" height="200" src="пластика век - блефаропластика.jpg"><?=$key["description_" . $_SESSION["NowLang"] . ""]?></div>
+				<a href="../../contact/index.php"><div class="btn btn-danger"><?php foreach($db->operationsButtonsTitle() as $key_1) print $key_1["title_" . $_SESSION["NowLang"] . ""];?></div></a>
+			</div>
+
+
+			
+
+
+		<?php } } ?>
+
+			
+
+		</div>
+	</div>
+
+
+<?php include "../footer.php"; ?>
