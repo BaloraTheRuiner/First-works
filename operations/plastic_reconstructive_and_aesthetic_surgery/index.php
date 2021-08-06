d07e66d98c0748c326940763c1600e9d51fdc691
Hayk Yolyan <54978371+BaloraTheRuiner@users.noreply.github.com>
2021-08-06 16:53:09 +0400

    Add files via upload

---
+++ operations/plastic_reconstructive_and_aesthetic_surgery/index.php
@@ -0,0 +1,41 @@
+<?php include "../header.php"; ?>
+
+			
+	<div class="wrap_div">
+		<div class="wrap_diamond operations_wrap_diamond">
+			<div class="recons_diamond_icon wrap_diamond_icon oper_icon"></div>
+		</div>
+		<?php foreach ($db->operationsTitles() as $key){ if($key["id"] == 12){ ?>
+		<div class="wrap_line"><b><?=$key["title_" . $_SESSION["NowLang"] . ""]?></b></div>
+	</div>
+
+	<div class="operations">
+		<div class="inner_operations">
+
+
+
+
+			<div class="operations_content">
+				<div class="operations_content_div">
+					<?=$key["description_" . $_SESSION["NowLang"] . ""]?>
+					<div class="reconstructive">
+						<img src="До-и-После.jpg">
+						<img src="До-И-После1.jpg">
+					</div>
+				</div>
+				<a href="../../contact/index.php"><div class="btn btn-danger"><?php foreach($db->operationsButtonsTitle() as $key_1) print $key_1["title_" . $_SESSION["NowLang"] . ""];?></div></a>
+			</div>
+
+
+			
+
+
+		<?php } } ?>
+
+			
+
+		</div>
+	</div>
+
+
+<?php include "../footer.php"; ?>
